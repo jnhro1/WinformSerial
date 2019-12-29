301ce045815a6f161d2ed376ee7b9d69b5be5cac
Jonahyun <jnhro1@gmail.com>
2019-12-29 18:14:04 +0900

    CREATE FILE

---
+++ WinformTest1/WinformTest1/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace WinformTest1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button_PortOpen = new System.Windows.Forms.Button();
+            this.button_PortClose = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.SerialChart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.SerialChart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "COM Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Baud Rate";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(28, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 26);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "115200",
+            "38400",
+            "19200",
+            "9600"});
+            this.comboBox2.Location = new System.Drawing.Point(234, 33);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(161, 26);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // button_PortOpen
+            // 
+            this.button_PortOpen.Location = new System.Drawing.Point(418, 20);
+            this.button_PortOpen.Name = "button_PortOpen";
+            this.button_PortOpen.Size = new System.Drawing.Size(89, 44);
+            this.button_PortOpen.TabIndex = 4;
+            this.button_PortOpen.Text = "열기";
+            this.button_PortOpen.UseVisualStyleBackColor = true;
+            this.button_PortOpen.Click += new System.EventHandler(this.button_PortOpen_Click);
+            // 
+            // button_PortClose
+            // 
+            this.button_PortClose.Location = new System.Drawing.Point(522, 20);
+            this.button_PortClose.Name = "button_PortClose";
+            this.button_PortClose.Size = new System.Drawing.Size(78, 44);
+            this.button_PortClose.TabIndex = 5;
+            this.button_PortClose.Text = "닫기";
+            this.button_PortClose.UseVisualStyleBackColor = true;
+            this.button_PortClose.Click += new System.EventHandler(this.button_PortClose_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("굴림체", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.richTextBox1.Location = new System.Drawing.Point(28, 73);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(572, 248);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(668, 30);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(136, 40);
+            this.richTextBox2.TabIndex = 10;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox2_KeyPress_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 9F);
+            this.label3.Location = new System.Drawing.Point(665, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 18);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Input Data";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Font = new System.Drawing.Font("굴림체", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.richTextBox3.Location = new System.Drawing.Point(668, 76);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(531, 245);
+            this.richTextBox3.TabIndex = 12;
+            this.richTextBox3.Text = "";
+            // 
+            // SerialChart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.SerialChart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.SerialChart1.Legends.Add(legend1);
+            this.SerialChart1.Location = new System.Drawing.Point(28, 362);
+            this.SerialChart1.Name = "SerialChart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.SerialChart1.Series.Add(series1);
+            this.SerialChart1.Size = new System.Drawing.Size(1171, 300);
+            this.SerialChart1.TabIndex = 13;
+            this.SerialChart1.Text = "SerialChart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1275, 693);
+            this.Controls.Add(this.SerialChart1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button_PortClose);
+            this.Controls.Add(this.button_PortOpen);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SerialChart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button_PortOpen;
+        private System.Windows.Forms.Button button_PortClose;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart SerialChart1;
+    }
+}
+
---
+++ WinformTest1/WinformTest1/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Linq;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Text.RegularExpressions;
+
+
+
+namespace WinformTest1
+{
+    public partial class Form1 : Form
+
+    {
+        SerialPort serialPort1 = new SerialPort();
+        delegate void SetTextCallback(string opt);
+        //string RBuffer; // 수신버퍼로 사용할 문자열 변수선언
+        private Thread testThread;
+        private double[] cpuArray = new double[6000];
+
+        private Point currentMouseLcation = Point.Empty; // 현재 마우스 위치
+        private RectangleF plotArea = RectangleF.Empty; // InnerPlotPostion
+
+
+
+        Stopwatch sw = new Stopwatch(); // 경과시간 측정을 위한 stopwatch 선언
+
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived);
+        }
+
+        private void update_serial_combobox()
+        {
+            string[] portsArray = SerialPort.GetPortNames();
+
+            comboBox1.Items.Clear();
+
+            foreach (string portnumber in portsArray)
+            {
+                comboBox1.Items.Add(portnumber);
+            }
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            update_serial_combobox();
+        }
+
+        private void button_PortOpen_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Close();
+            }
+
+            if (comboBox1.SelectedItem == null)
+            {
+                return;
+            }
+
+            serialPort1.PortName = comboBox1.SelectedItem.ToString();
+            serialPort1.BaudRate = Int32.Parse(comboBox2.SelectedItem.ToString());
+
+            try
+            {
+                serialPort1.Open();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("포트가 이미 열려 있습니다.");
+            }
+
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.DiscardInBuffer();
+
+                button_PortOpen.Enabled = false;
+                button_PortClose.Enabled = true;
+
+                comboBox1.Enabled = false;
+                comboBox2.Enabled = false;
+
+                richTextBox2.Enabled = true;
+
+                SerialChart1.Series[0].Points.Clear(); // 시리얼차트에 찍힌 데이터포인트를 클리어한다.
+                SerialChart1.ChartAreas[0].AxisX.Minimum = sw.ElapsedMilliseconds; // 시리얼차트의 x축 최소값을 스탑워치의 현재시간값으로 설정.
+                SerialChart1.ChartAreas[0].AxisX.Maximum = sw.ElapsedMilliseconds + 5000; // 시리얼차트의 x축 최대값을 스탑워치의 '현재시간 + 5000'값으로 설정.
+
+                ////////////////////////////////////////////////////////////////////////////////
+                
+
+
+                 SerialChart1.ChartAreas[0].AxisX.ScrollBar.Enabled = true;
+                 SerialChart1.ChartAreas[0].AxisX.ScrollBar.Size = 20;
+                 SerialChart1.ChartAreas[0].AxisX.ScrollBar.ButtonStyle =
+                     System.Windows.Forms.DataVisualization.Charting.ScrollBarButtonStyles.All;
+                 SerialChart1.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+       
+                this.SerialChart1.ChartAreas[0].CursorX.AutoScroll = true;
+
+                this.SerialChart1.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+                this.SerialChart1.ChartAreas[0].CursorY.IsUserEnabled = true;
+                this.SerialChart1.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+                
+                 SerialChart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+                 SerialChart1.ChartAreas[0].CursorX.IsUserEnabled = true;
+                 SerialChart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+                 this.SerialChart1.ChartAreas[0].CursorX.Interval = 5;
+                // 커서 선택 단위를 막대그래프 한개씩 함.
+
+                SerialChart1.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+                SerialChart1.ChartAreas[0].CursorY.IsUserEnabled = true;
+                SerialChart1.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+                this.SerialChart1.ChartAreas[0].CursorY.Interval = 50;
+                SerialChart1.ChartAreas[0].AxisY.Interval = 0;
+
+                sw.Start(); // 정의된 스탑워치 sw를 통해 경과시간측정을 시작한다.
+
+                testThread = new Thread(new ThreadStart(this.getPerformanceCounters));
+                //testThread.IsBackground = true;
+                testThread.Start();
+
+            }
+        }
+
+        private void button_PortClose_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+
+            button_PortOpen.Enabled = true;
+            button_PortClose.Enabled = false;
+
+            comboBox1.Enabled = true;
+            comboBox2.Enabled = true;
+
+            richTextBox2.Enabled = false;
+            SerialChart1.Enabled = false;
+
+            sw.Stop();
+            //sw.Reset(); // 경과시작측정을 중지하고 경과시작을 다시 0으로 설정.
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox2.SelectedIndex = 0;
+
+            button_PortOpen.Enabled = true;
+            button_PortClose.Enabled = false;
+            comboBox1.Enabled = true;
+            comboBox2.Enabled = true;
+            richTextBox2.Enabled = false;
+        }
+
+         private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+         {
+             int i_recv_size = serialPort1.BytesToRead;
+             byte[] b_tmp_buf = new byte[i_recv_size];
+             string recv_str = "";
+
+
+             serialPort1.Read(b_tmp_buf, 0, i_recv_size);
+
+
+             recv_str = Encoding.Default.GetString(b_tmp_buf);
+ 
+
+             string strValue = recv_str;
+             strValue = System.Text.RegularExpressions.Regex.Replace(recv_str, @"[^\d]", "");
+            double data;
+
+            data = Convert.ToDouble(strValue);
+
+            for (int i=0; i< 6000; i++)
+             {
+               // if (data <= 0) break;
+                cpuArray[i] = data;
+             }
+
+            this.BeginInvoke(new SetTextCallback(display_data), new object[] { recv_str });
+             this.BeginInvoke(new SetTextCallback(str2hex), new object[] { recv_str });
+
+         }
+
+     
+        private void display_data(string str)
+        {
+            richTextBox1.Text += str;
+            richTextBox1.SelectionStart = richTextBox1.TextLength;
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void str2hex(string strData)
+        {
+            string resultHex = string.Empty;
+            byte[] arr_byteStr = Encoding.Default.GetBytes(strData);
+
+            int cnt = 0;
+            int linecnt = 0;
+
+            foreach (byte byteStr in arr_byteStr)
+            {
+                resultHex += string.Format("{0:X2}" + " ", byteStr);
+                cnt++;
+                linecnt++;
+                resultHex = resultHex.Replace("0A", "0A\r\n");
+            }
+
+            richTextBox3.Text += resultHex;
+            richTextBox3.SelectionStart = richTextBox3.TextLength;
+            richTextBox3.ScrollToCaret();
+
+        }
+
+
+        private void richTextBox2_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+                return;
+
+            char[] buffer = new char[1];
+            buffer[0] = e.KeyChar;
+
+            serialPort1.Write(buffer, 0, buffer.Length);
+            richTextBox2.Text = "";
+        }
+
+        public static int GetNumberOnly(string _strInput)
+
+        {
+            string strValue = System.Text.RegularExpressions.Regex.Replace(_strInput, @"[^\d]", "");
+            int nResult = -1;
+
+            if (int.TryParse(strValue, out nResult) == false)
+            {
+                return nResult;
+            }
+            return nResult;
+
+        }
+
+
+        private void getPerformanceCounters()
+        {
+            while (true)
+            {
+
+                //Array.Copy(cpuArray, 1, cpuArray, 0, cpuArray.Length - 1);
+
+                  if (SerialChart1.IsHandleCreated)
+                   {
+                       this.Invoke((MethodInvoker) delegate { UpdateChart(); });
+                   }
+                   else
+                   {
+                       //......
+                   }
+
+            Thread.Sleep(600);
+            }
+        }
+
+        private void UpdateChart()
+        {
+            //SerialChart1.Series["Series1"].Points.Clear();
+
+             for (int i = 0; i < cpuArray.Length - 1; ++i)
+             {
+                 SerialChart1.Series[0].Points.AddXY(sw.ElapsedMilliseconds, cpuArray[i]); // SerialChart1에 x축을 경과시간, y축을 RDec로 설정하여 포인트를 추가한다.
+             }
+
+            if (SerialChart1.Series[0].Points.Count > 0) // SerialChart1에 Series Point가 존재할 경우
+            {
+                while (SerialChart1.Series[0].Points[0].XValue < sw.ElapsedMilliseconds - 5000) // X축값이 경과시간으로부터 5000밀리초 이하의 값인 동안
+                {
+                    SerialChart1.Series[0].Points.RemoveAt(0); // SerialChart1의 첫포인트들을 제거한다.
+
+                       SerialChart1.ChartAreas[0].AxisX.Minimum = SerialChart1.Series[0].Points[0].XValue; // 차트의 포인트 첫값을 X축의 최소값으로 설정.
+                       SerialChart1.ChartAreas[0].AxisX.Maximum = sw.ElapsedMilliseconds+100; // '경과시간 + 100밀리초'를 X축의 최대값으로 설정
+                }
+            }
+        }
+    }
+}
